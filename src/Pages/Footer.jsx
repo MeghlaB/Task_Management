d1bb128675086446e27533584df4dbbc9e55e96a
MeghlaB <meghlab964@gmail.com>
2025-02-21 10:46:50 +0600

    edit

--- src/Pages/Footer.jsx
+++ src/Pages/Footer.jsx
@@ -5,7 +5,7 @@ export default function Footer() {
     <div>
       <footer className="footer footer-center bg-base-300 text-base-content pb-3.5 p-4">
   <aside>
-    <p>Copyright © {new Date().getFullYear()} - All right reserved by ACME Industries Ltd</p>
+    <p>Copyright © {new Date().getFullYear()} - All right reserved by Task manager Industries Ltd</p>
   </aside>
 </footer>
     </div>
